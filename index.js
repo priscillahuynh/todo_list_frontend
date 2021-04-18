9444fd715abd33a08ba72327e7116b4dc9a87b58
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-18 10:41:14 +0400

    fetch items and render to DOM

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("in index.js")
+const endPoint = "http://localhost:3000/api/v1/items"
+
+document.addEventListener('DOMContentLoaded', () => {
+    getItems()
+})
+
+function getItems() {
+    fetch(endPoint)
+    .then(r => r.json())
+    .then(items => {
+        items.data.forEach(item => {
+            const itemsMarkup = `
+            <div data-id=${item.id}> 
+            <h3>${item.attributes.description}</h3>
+            <p>${item.attributes.list.title}</p>
+            <button data-id=${item.id}>Delete</button>
+            </div>
+            <br>` ;
+
+            document.querySelector('#item-container').innerHTML+= itemsMarkup
+        })
+    })
+}
+
