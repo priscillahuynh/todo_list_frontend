8f413e0f560d9bc5ab94217abae571c64a8146fe
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-19 15:37:28 +0400

    add event listener to add item form

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const endPoint = "http://localhost:3000/api/v1/lists"
 
+
 document.addEventListener('DOMContentLoaded', () => {
     getLists()
+    
 })
 
 function getLists() {
@@ -11,29 +13,41 @@ function getLists() {
         lists.data.forEach(list => {
             const list_container = document.createElement("div")
             document.body.appendChild(list_container)
-            list_container.id = list.id
+            list_container.id = list.attributes.title
             list_container.innerHTML+= `<h3>${list.attributes.title}</h3>`
             list.attributes.items.forEach(item => {
                 const itemsMarkup = `
-                    <i class="far fa-circle"></i>
-                    ${item.description}
-                    <i class="fas fa-trash-alt"></i>
-                    <br>` 
-
+                <i class="far fa-circle"></i>
+                ${item.description}
+                <i class="fas fa-trash-alt"></i>
+                <br>` 
+                
                 list_container.innerHTML+= itemsMarkup 
             })
-
+            
             const form_container = document.createElement("div")
+            form_container.class = "form"
             form_container.innerHTML = `
-                <br>
-                <form id="add-item">
-                <input type="hidden", name="list_id", value="${list.id}">
-                <input type="text" name="description" placeholder="Add new item">
-                <input type="submit" name="submit">
-                </form>`
-
+            <br>
+            <form class="add-item">
+            <input type="hidden", name="list_id", value="${list.id}">
+            <input type="text" name="description" placeholder="Add new item">
+            <button type="submit" name="submit">Submit</button>
+            </form>`
+            
             list_container.appendChild(form_container)
         })
+        itemForms = document.getElementsByClassName("add-item")
+        for (var i = 0; i < itemForms.length; i ++) {
+        itemForms[i].addEventListener("submit", (e) => handleSubmit(e))
+    }
     })
 }
+
+
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e)
+}
+
 
