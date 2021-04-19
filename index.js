4d66b2a9b3f6a286e5b3f9552084808797fa4845
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-19 10:19:01 +0400

    update html and js to render items for each list

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const endPoint = "http://localhost:3000/api/v1/items"
+const endPoint = "http://localhost:3000/api/v1/lists"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getItems()
+    getLists()
 })
 
-function getItems() {
+function getLists() {
     fetch(endPoint)
     .then(r => r.json())
-    .then(items => {
-        items.data.forEach(item => {
-            const itemsMarkup = `
-            <div data-id=${item.id}> 
-            <h3>${item.attributes.description}</h3>
-            <p>${item.attributes.list.title}</p>
-            <button data-id=${item.id}>Delete</button>
-            </div>
-            <br>` ;
+    .then(lists => {
+        lists.data.forEach(list => {
+            const listMarkup = `
+            <div data-id=${list.id}> 
+            <h3>${list.attributes.title}</h3>
+            </div> ` ;
+            document.querySelector('#list-container').innerHTML+= listMarkup
 
-            document.querySelector('#item-container').innerHTML+= itemsMarkup
+            list.attributes.items.forEach(item => {
+                const itemsMarkup = `
+                <p>${item.description}</p>
+                `
+                document.querySelector('#list-container').innerHTML+= itemsMarkup
+            })
         })
     })
 }
